6d08bac33763932a9894f8b39f28919e36a8a399
prateekganguly1998 <gangulyprateek0@gmail.com>
2020-08-18 13:31:45 +0530

    day 22

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const helpController=require('../controllers/admin/help');
+const uploadService=require('../services/upload');
 const { validator } = require("../middleware/validator");
 const isAuth = require("../middleware/isAuth");
 
 router.post("/updateHelpCategory",helpController.updateHelpCategories);
-
+router.post('/testUpload',uploadService.testUpload);
 
 module.exports = router;
